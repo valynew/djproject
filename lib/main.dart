a2fcd51105cfcfb017c5b539ba9ebbc83e5b212f
valynew <irasubizaalyne@gmail.com>
2025-04-25 17:29:12 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:myfirst_project/hometabular.dart';
+import 'package:myfirst_project/hometabular.dart'; // use the correct import path
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Name Collector',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
       debugShowCheckedModeBanner: false,
-      home: const Hometabular(),
+      title: 'DJ App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Hometabular(),  // <-- Make sure this points to your form version
     );
   }
 }
-
-
-
-
-
